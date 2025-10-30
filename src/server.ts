c673fb0fdfd268a13d02474f2062fa5212031bde
Shivam Singh <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2025-10-30 16:22:48 +0530

    web socker broadcasting advance version

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket:WebSocket) => {
+wss.on("connection", (socket: WebSocket) => {
   console.log(`new user has joined`);
+  console.log(`active users count now is :${wss.clients.size}`);
 
   socket.on("message", (message) => {
     console.log(`message received from client is ${message.toString()}`);
@@ -11,10 +12,19 @@ wss.on("connection", (socket:WebSocket) => {
     // setTimeout(() => {
     //   socket.send(`server received your message: ${message.toString()}`);
     // }, 2000);
-
+    // broadcast to all other clients and it self
+    
     wss.clients.forEach((client) => {
-      if (client !== socket && client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN) {
         client.send(`broadcast message: ${message.toString()}`);
+      }
+    });
+  });
+
+  socket.on("close", () => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(`a user has left. active users count: ${wss.clients.size}`);
       }
     });
   });
